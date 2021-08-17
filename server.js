da4494f1727fddcad7274c38a842d1d20ba4100c
Ben Cook <mcbeanoo@gmail.com>
2021-08-17 17:23:29 +1000

    add hello routes

--- server.js
+++ server.js
@@ -7,7 +7,17 @@ const port = 8000;
 
 // Set a route for GET /
 app.get("/", (req, res) => {
-  res.json({ testData: "test" }).status(201);
+  res.json({ testData: "test" }).status(200);
+});
+
+// Set a route for GET /hello
+app.get("/hello", (req, res) => {
+  res.send("Hello!").status(200);
+});
+
+// Set a route for GET /hello/:name
+app.get("/hello/:name/", (req, res) => {
+  res.send(`Hello ${req.params.name}`).status(200);
 });
 
 // Start listening on a specific port
